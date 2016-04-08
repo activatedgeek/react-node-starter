03e208f014d0e01363bf6eeeebfa9afb83c60598
Sanyam Kapoor <1sanyamkapoor@gmail.com>
2016-04-08 13:25:19 +0530

    added lots of missing configurations

--- config/app.js
+++ config/app.js
@@ -1,6 +1,6 @@
 const config = {
   env: process.env.NODE_ENV || 'production',
-  port: process.env.PORT || '5000',
+  port: process.env.PORT || '3000',
   appKey: process.env.APP_KEY || 'unique-app-key',
   redis: {
     host: process.env.REDIS_HOST || '127.0.0.1',
